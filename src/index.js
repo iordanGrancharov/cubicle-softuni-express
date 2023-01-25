7ffc7685ab600bc71c756bf3024fba8b378ca2f6
Iordan <iamironadicted@gmail.com>
2023-01-25 23:39:48 +0200

    commiting changes to the main file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
+const setupHandlebars = require("./config/setupViewEngine");
 const config = require("./config/config");
+const router = require("./router/router");
 
 const app = express();
+
+setupHandlebars(app);
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-
-app.engine("hbs", handlebars.engine());
-app.set("view engine", "hbs");
-app.set("views", "./views");
-
-app.get("/", (req, res) => {
-  res.send("home");
-});
+app.use(router);
 
 app.listen(config.port, () =>
   console.log(`Listening on port ${config.port}! Now its up to you...`)
